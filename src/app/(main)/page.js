88cd6b0fc4b8f9daa04df82f40a68b06ed4a82d0
Mohammed Rakib <mohammedrakibhossain02@gmail.com>
2026-06-15 14:44:24 +0600

    feat: add grid layout and category fetching

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,3 +1,39 @@
-export default function Home() {
-  return <h2 className="text-5xl">Home Page</h2>;
+const getCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories",
+  );
+
+  const data = await res.json();
+
+  return data.data;
+};
+
+export default async function Home() {
+  const categories = await getCategories();
+
+  console.log(categories.news_category, "category data");
+
+  return (
+    <div className="container mx-auto mt-5 grid grid-cols-12 gap-4">
+      <div className="col-span-3  ">
+        <h2 className="text-lg font-bold">All Categories</h2>
+        <ul className="flex flex-col gap-3 mt-6">
+          {categories.news_category.map((category) => (
+            <li
+              className="bg-slate-100 py-2 rounded-md font-bold text-center text-md"
+              key={category.category_id}
+            >
+              {category.category_name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="col-span-6 bg-red-500 text-3xl font-bold">All News</div>
+
+      <div className="col-span-3 bg-blue-500 text-3xl font-bold">
+        Social Icons
+      </div>
+    </div>
+  );
 }
--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -5,27 +5,27 @@ const news = [
   {
     id: 1,
     title:
-      "Breaking News: Nvidia reaches new milestones as AI demand continues to surge",
+      "Breaking News: Nvidia reaches new milestones as AI demand continues to surge !!",
   },
   {
     id: 2,
     title:
-      "Breaking News: Global stock markets rally following positive economic data",
+      "Breaking News: Global stock markets rally following positive economic data !!",
   },
   {
     id: 3,
     title:
-      "Breaking News: SpaceX successfully advances next-generation Starship testing",
+      "Breaking News: SpaceX successfully advances next-generation Starship testing !!",
   },
   {
     id: 4,
     title:
-      "Breaking News: Renewable energy investments hit record levels worldwide",
+      "Breaking News: Renewable energy investments hit record levels worldwide !!",
   },
   {
     id: 5,
     title:
-      "Breaking News: Major breakthrough announced in cancer treatment research",
+      "Breaking News: Major breakthrough announced in cancer treatment research !!",
   },
 ];
 
